c8a2c1c0ae538712517fe2d4f7263c78a49f327c
Utkarsh <132635254+karshTD@users.noreply.github.com>
2025-11-05 22:12:58 +0530

    Create areas.cpp

---
+++ oop/constructor overloading/areas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class area(){
+  public:
+area(double r){
+cout<< "circle area: << 3.14 * r * r << endl;
+  }
+
+area(double l, double w){
+cout<< "rectangle area: "<< l * w << endl;
+}
+
+area(double b, double h,){
+cout<< "triangle area: "<< 0.5 * b * h <<endl;
+}
+};
