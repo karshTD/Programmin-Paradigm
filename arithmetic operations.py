aa01a96df31ca66927c07aadec8735d6b7c9cb2e
Utkarsh <132635254+karshTD@users.noreply.github.com>
2025-12-09 15:30:10 +0530

    Add arithmetic operations for addition, subtraction, and product

---
+++ arithmetic operations.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    def addition():
+        print(a + b)
+
+    def subtraction():
+        print(a - b)
+
+    def product():
+        print(a * b)
+
+    addition()
+    subtraction()
+    product()
